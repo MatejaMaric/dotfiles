97f076b23b507afea8981de63a9f516d8d8054a1
Mateja <mail@matejamaric.com>
2020-09-03 00:26:35 +0200

    auto float pin entry

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -179,6 +179,7 @@ follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
+    {'wmclass': 'pinentry-gtk-2'},
     # Run the utility of `xprop` to see the wm class and name of an X client.
     {'wmclass': 'confirm'},
     {'wmclass': 'dialog'},

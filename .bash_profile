7510d92ad45ff503fe64719209dad9568b2e57eb
Mateja <mail@matejamaric.com>
2021-02-08 12:00:27 +0100

    Add kaychain, remove systemd startx condition, update and comment autostart.sh

--- .bash_profile
+++ .bash_profile
@@ -1,9 +1,7 @@
-#
-# ~/.bash_profile
-#
-
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
-if systemctl -q is-active graphical.target && [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+eval $(keychain --clear --eval --agents gpg,ssh --quiet)
+
+if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
 	  startx
 fi
--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 xset -b
+
 setxkbmap -option caps:swapescape
 #setxkbmap -layout us,rs -variant ,alternatequotes -option grp:alt_shift_toggle,caps:swapescape
+
 numlockx &
 dunst &
 udiskie &
+
+#picom &
 compton --backend glx &
+
 feh --bg-fill ~/stuff/pictures/alps-house.jpg
-gpg-agent &
-ssh-agent &
+
+#lxpolkit &
+#gpg-agent &
+#ssh-agent &
+
 mpd &

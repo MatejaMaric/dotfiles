d2e46f939651f716fe26e526eb6a57b6ed4ec4b4
Mateja <mail@matejamaric.com>
2021-02-23 18:58:27 +0100

    Use alias in `get_password.sh` script.

--- .local/bin/get_password.sh
+++ .local/bin/get_password.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-pass -c $(find $HOME/.password-store -type f -iname *gpg | sed 's/.*password-store\/\(.*\).gpg/\1/' | sort | rofi -dmenu)
+
+shopt -s expand_aliases
+# alias list_passwords='dmenu'
+alias list_passwords='rofi -dmenu'
+
+pass -c $(find $HOME/.password-store -type f -iname *gpg | sed 's/.*password-store\/\(.*\).gpg/\1/' | sort | list_passwords)

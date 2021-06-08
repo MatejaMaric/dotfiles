68c0c3eb36359af1cca560829c6a5a39abc51345
Mateja <mail@matejamaric.com>
2021-06-08 21:40:02 +0200

    Add status bar test script.

---
+++ .local/bin/sb_test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+case $BUTTON in
+	# Left mouse click
+	1) notify-send "$BUTTON" ;;
+
+	# Middle mouse click
+	2) notify-send "$BUTTON" ;;
+
+	# Right mouse click
+	3) notify-send "$BUTTON" ;;
+
+	# Mouse wheel up
+	4) notify-send "$BUTTON" ;;
+
+	# Mouse wheel down
+	5) notify-send "$BUTTON" ;;
+esac
+
+echo "Test"
